4d531b1f069b613f436f54c5325f333c24c2c24d
Ty Tanita <ty.tanita@warnerbros.com>
2022-08-10 13:34:01 -0700

    spelling fix

--- hello world.py
+++ hello world.py
@@ -1,5 +1,5 @@
 print('Hello there it is 8/10/22 at 1.18pm')
 import pandas as pd
-#The problem is that pnadas is not importing.
+#The problem is that pandas is not importing.
 data = pd.read_csv("moviedata.csv")
 data.head()
